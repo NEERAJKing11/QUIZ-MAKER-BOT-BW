cc1c19b3b93d65ab9b6948fe0abed9bb0b3fc299
NEERAJKing11 <neerajking3605@gmail.com>
2025-12-30 15:00:27 +0530

    Change bot API token in config.py
    Updated API token for the bot.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import telebot
 
 # अपनी बॉट टोकन यहाँ डालें
-API_TOKEN = 'YOUR_BOT_TOKEN_HERE'
+API_TOKEN = '7965361903:AAExpjboakayOxURWG47AnspkeptzqBFDo0'
 
 # बॉट ऑब्जेक्ट बनाना
 bot = telebot.TeleBot(API_TOKEN)
