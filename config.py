62f3dc366689e9d0d2449205fc3c46963b5589c5
NEERAJKing11 <neerajking3605@gmail.com>
2025-12-30 15:03:10 +0530

    Replace bot token and add MongoDB URI
    Updated API_TOKEN and added MONGO_URI placeholders.

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
 import telebot
 
-# अपनी बॉट टोकन यहाँ डालें
-API_TOKEN = '7965361903:AAExpjboakayOxURWG47AnspkeptzqBFDo0'
+# 1. अपना टेलीग्राम बॉट टोकन यहाँ डालें
+API_TOKEN = 'YOUR_BOT_TOKEN_HERE'
 
-# बॉट ऑब्जेक्ट बनाना
+# 2. अपना MongoDB URL यहाँ डालें
+# यह कुछ ऐसा दिखता है: "mongodb+srv://user:password@cluster..."
+MONGO_URI = 'YOUR_MONGODB_CONNECTION_STRING_HERE'
+
 bot = telebot.TeleBot(API_TOKEN)
