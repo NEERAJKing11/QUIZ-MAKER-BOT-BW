511449acbb72b7b8d2fbb3e94f10cc2702730295
NEERAJKing11 <neerajking3605@gmail.com>
2025-12-30 15:17:27 +0530

    Update API_TOKEN and MONGO_URI in config.py
    Updated API token and MongoDB connection string.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import telebot
 
 # 1. अपना टेलीग्राम बॉट टोकन यहाँ डालें
-API_TOKEN = 'YOUR_BOT_TOKEN_HERE'
+API_TOKEN = '7965361903:AAExpjboakayOxURWG47AnspkeptzqBFDo0'
 
 # 2. अपना MongoDB URL यहाँ डालें
 # यह कुछ ऐसा दिखता है: "mongodb+srv://user:password@cluster..."
-MONGO_URI = 'YOUR_MONGODB_CONNECTION_STRING_HERE'
+MONGO_URI = 'mongodb+srv://sardakumari905_db_user:p6yUp70gQGLwYTDN@cluster0.0r1bebe.mongodb.net/?appName=Cluster0'
 
 bot = telebot.TeleBot(API_TOKEN)
